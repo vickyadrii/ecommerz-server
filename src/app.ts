d9412e70eb1ebc212f18b4c4c2c31657a95d5df6
vickyadrii <vickyadri103@gmail.com>
2025-07-23 01:15:19 +0700

    fix: resolve cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors"; // ⬅️ tambahkan ini
 import productRoutes from "./routes/products.route.js";
 import adjustmentRoutes from "./routes/adjustments.route.js";
 
+const allowedOrigins = process.env.FRONTEND_URL?.split(",").map((origin) => origin.trim()) || [];
+console.log("Allowed Origins:", allowedOrigins);
+
 const app = Fastify({ logger: true });
+
+await app.register(cors, {
+  origin: allowedOrigins,
+  credentials: true,
+});
 
 app.register(productRoutes, { prefix: "/products" });
 app.register(adjustmentRoutes, { prefix: "/adjustments" });
