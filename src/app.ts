5bf1cb4168af502b8475d8302b8211724c42c448
vickyadrii <vickyadri103@gmail.com>
2025-07-23 10:12:57 +0700

    chore: adding api methods

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ const app = Fastify({ logger: true });
 await app.register(cors, {
   origin: allowedOrigins,
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 });
 
 app.register(productRoutes, { prefix: "/products" });
